912479c08b26b57abeb7a5634af976642bf8daf8
Abu Nahid <abunahid767@gmail.com>
2023-08-11 11:11:41 +0600

    third commt

---
+++ app/page.js
@@ -0,0 +1,74 @@
+"use client";
+
+import Banner from "@/components/Banner";
+import Image from "next/image";
+import requests from "../uitls/request";
+import { useEffect, useState } from "react";
+import Row from "@/components/Row";
+
+export default function Home() {
+  const [netflixOriginals, setnetflixOriginals] = useState([]);
+  const [trendingNow, settrendingNow] = useState([]);
+  const [topRated, settopRated] = useState([]);
+  const [actionMovies, setactionMovies] = useState([]);
+  const [comedyMovies, setcomedyMovies] = useState([]);
+  const [horrorMovies, sethorrorMovies] = useState([]);
+  const [romanceMovies, setromanceMovies] = useState([]);
+  const [documentaries, setdocumentaries] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(requests.fetchNetflixOriginals);
+        const trending = await fetch(requests.fetchTrending);
+        const topretad = await fetch(requests.fetchTopRated);
+        const action = await fetch(requests.fetchActionMovies);
+        const horror = await fetch(requests.fetchHorrorMovies);
+        const romance = await fetch(requests.fetchRomanceMovies);
+        const documetory = await fetch(requests.fetchDocumentaries);
+        const comedy = await fetch(requests.fetchComedyMovies);
+
+        const jsonData = await response.json();
+        const newcomedy = await comedy.json();
+        const newrending = await trending.json();
+        const newtopretad = await topretad.json();
+        const newaction = await action.json();
+        const newhorror = await horror.json();
+        const newrpmance = await romance.json();
+        const newdocomentory = await documetory.json();
+
+        setnetflixOriginals(jsonData.results);
+
+        setcomedyMovies(newcomedy.results);
+        settrendingNow(newrending.results);
+        settopRated(newtopretad.results);
+        setactionMovies(newaction.results);
+        sethorrorMovies(newhorror.results);
+        setromanceMovies(newrpmance.results);
+        setdocumentaries(newdocomentory.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+      <Banner netflixOriginals={netflixOriginals} />
+
+      <section className="md:space-y-24">
+        <Row title="Trending Now" movies={trendingNow} />
+        <Row title="Top Rated" movies={topRated} />
+        <Row title="Action Thrillers" movies={actionMovies} />
+        {/* My List Component */}
+
+        <Row title="Comedies" movies={comedyMovies} />
+        <Row title="Scary Movies" movies={horrorMovies} />
+        <Row title="Romance Movies" movies={romanceMovies} />
+        <Row title="Documentaries" movies={documentaries} />
+      </section>
+    </main>
+  );
+}
---
+++ components/Header.js
@@ -0,0 +1,60 @@
+"use client";
+
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">TV Show</li>
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Movie</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden cursor-pointer h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <NotificationsNoneIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
